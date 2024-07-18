b6c6a9db954197954b8378478cd6c7b697e2b04c
Dikshith Shetty <shetty.dikshith21@gmail.com>
2024-07-19 03:25:09 +0400

    Product controller tests

--- src/main/java/com/project/retail_site/controllers/ProductController.java
+++ src/main/java/com/project/retail_site/controllers/ProductController.java
@@ -1,10 +1,12 @@
 package com.project.retail_site.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.retail_site.entities.Product;
@@ -26,6 +28,7 @@ public class ProductController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Product createProduct(@RequestBody Product product) {
         return productService.saveProduct(product);
     }
--- src/test/java/com/project/retail_site/controllers/ProductControllerTest.java
+++ src/test/java/com/project/retail_site/controllers/ProductControllerTest.java
@@ -1,33 +1,76 @@
-// package com.project.retail_site.controllers;
+package com.project.retail_site.controllers;
 
-// import org.junit.jupiter.api.BeforeAll;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-// import org.springframework.boot.test.mock.mockito.MockBean;
-// import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
-// import com.project.retail_site.entities.Product;
-// import com.project.retail_site.services.ProductService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-// @WebMvcTest(ProductController.class)
-// class ProductControllerTest {
-//     @Autowired
-// 	private MockMvc mockMvc;
+import com.project.retail_site.entities.Product;
+import com.project.retail_site.entities.User;
+import com.project.retail_site.enums.CategoryEnum;
+import com.project.retail_site.services.ProductService;
+import com.project.retail_site.utils.TestUtils;
 
-//     @MockBean
-//     private ProductService productService;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
-//     private Product product;
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+    @Autowired
+	private MockMvc mockMvc;
 
-//     @BeforeAll
-//     void setUp() {
-//         product = new Product();
-//         product.setId(1l);
-//         product.setName("Product1");
-//         product.setPrice(200.00d);
-//     }
+    @MockBean
+    private ProductService productService;
+
+    private static Product testProduct;
+
+    @BeforeAll
+    static void setUp() {
+        testProduct = new Product();
+        testProduct.setId(1l);
+        testProduct.setName("Product1");
+        testProduct.setPrice(200.00d);
+        testProduct.setCategory(CategoryEnum.OTHER);
+    }
+
+    @Test
+    void getProductTest() throws Exception {
+        when(productService.getProductById(anyLong())).thenReturn(testProduct);
+        this.mockMvc.perform(get("/products/{id}", 1))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id", is(equalTo(testProduct.getId())), Long.class))
+            .andExpect(jsonPath("$.name", is(testProduct.getName())))
+            .andExpect(jsonPath("$.category", is(testProduct.getCategory().toString())));
+    }
+
+    @Test
+    void createProductTest() throws Exception {
+        when(productService.saveProduct(any(Product.class))).thenReturn(testProduct);
+        this.mockMvc.perform(post("/products")
+                    .content(TestUtils.asJsonString(testProduct))
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isCreated())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                    .andExpect(jsonPath("$.id", is(equalTo(testProduct.getId())), Long.class))
+                    .andExpect(jsonPath("$.name", is(testProduct.getName())))
+                    .andExpect(jsonPath("$.category", is(testProduct.getCategory().toString())));
+    }
 
 
-// }
+}
 
 
